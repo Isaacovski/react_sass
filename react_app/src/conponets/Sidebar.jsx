59b9574f7075bf6bcb72f68613e1d67f6efcc5fc
Isaac gabriel <codebiisaac@gmail.com>
2023-04-08 02:54:48 -0300

    bug na img Avatar concerto

--- react_app/src/conponets/Sidebar.jsx
+++ react_app/src/conponets/Sidebar.jsx
@@ -1,9 +1,15 @@
+import Avatar from "../img/eu.jpg";
 
+import '../styles/conponets/sidebar.sass';
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  return <aside id="sidebar"> 
+  <img src= {Avatar} alt="Isaac Gabriel" />
+  <p className="title">desenvolvedor</p>
+  <p>redes sociais</p>
+  <p>informacoes de contato</p>
+  <a href="" className="btn">download curriculo</a>
+  </aside>;
+};
 
 export default Sidebar
