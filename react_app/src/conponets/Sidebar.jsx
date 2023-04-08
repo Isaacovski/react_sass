e2674567adb125cd8d8f06fbaa1584c5f0c1a8df
Isaac gabriel <codebiisaac@gmail.com>
2023-04-08 20:05:41 -0300

    adcionando os icones

--- react_app/src/conponets/Sidebar.jsx
+++ react_app/src/conponets/Sidebar.jsx
@@ -2,11 +2,13 @@ import Avatar from "../img/eu.jpg";
 
 import '../styles/conponets/sidebar.sass';
 
+import SocialNetworks from "./SocialNetworks";
+
 const Sidebar = () => {
   return <aside id="sidebar"> 
   <img src= {Avatar} alt="Isaac Gabriel" />
   <p className="title">desenvolvedor</p>
-  <p>redes sociais</p>
+  <SocialNetworks />
   <p>informacoes de contato</p>
   <a href="" className="btn">download curriculo</a>
   </aside>;
